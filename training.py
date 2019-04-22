97aa7376fd954daa16a873b211a68aedeb7cf83b
Micheal <micheal@swapapp.co>
2019-04-22 11:47:27 -0400

    Adding Training File

---
+++ training.py
@@ -0,0 +1,233 @@
+
+from __future__ import print_function, division
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+
+
+
+
+
+def main():
+
+
+
+    #Get Settings for Model
+
+    model_0, criterion, optimizer_conv, exp_lr_scheduler = getDefaultSettingsForModel()
+
+
+    ## Get path to dataset
+
+    path = input("Enter the directory to the dataset: ")
+
+
+    ########## Finally Train the Model Now #######
+    model = train_model(model_0, path, criterion, optimizer_conv,
+                         exp_lr_scheduler, num_epochs=100)
+
+
+    ### Save model
+    torch.save(model, "models/v0.1/model.pt")
+
+
+
+
+
+
+
+
+#Trains a given model
+def train_model(model, pathToData, criterion, optimizer, scheduler, num_epochs=25):
+
+
+    ### Prepare Data First
+    device, image_datasets, dataloaders, dataset_sizes, class_names = prepareData(pathToData)
+
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+def visualize_model(model, num_images=6):
+
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+    fig = plt.figure()
+
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloaders['val']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images//2, 2, images_so_far)
+                ax.axis('off')
+                ax.set_title('predicted: {}'.format(class_names[preds[j]]))
+                imshow(inputs.cpu().data[j])
+
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+        model.train(mode=was_training)
+
+
+
+
+##Normalizes the training data
+def prepareData(directory):
+
+    """
+    Prepares the training data
+
+    Normalizes the dataset for transfer learning. Also sets the device to the GPU if available
+
+    Parameters
+    ----------
+    directory : String
+        Path to the folder of data:
+
+"""
+    #Transformations on Data
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+
+    data_dir = directory
+
+    # The data directory contains the folders: 'train', and 'val'. Both train and val have folders for each class.
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                      for x in ['train', 'val']}
+
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=4)
+                  for x in ['train', 'val']}
+
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+
+    class_names = image_datasets['train'].classes
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    return device, image_datasets, dataloaders, dataset_sizes, class_names
+
+
+
+
+def getDefaultSettingsForModel():
+    #Gets Pretrained Model from Resnet18
+    model_conv = torchvision.models.resnet18(pretrained=True)
+
+    #inception_v3
+    #model_conv = torchvision.models.inception_v3(pretrained=True)
+
+    for param in model_conv.parameters():
+        param.requires_grad = False
+
+# Parameters of newly constructed modules have requires_grad=True by default
+    num_ftrs = model_conv.fc.in_features
+    model_conv.fc = nn.Linear(num_ftrs, 2)
+    model_conv = model_conv.to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
+    criterion = nn.CrossEntropyLoss()
+
+# Optimize only the final layer
+    optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+
+    return model_conv, criterion, optimizer_conv, exp_lr_scheduler
+
+
+
+
+#Runs Main
+main()
